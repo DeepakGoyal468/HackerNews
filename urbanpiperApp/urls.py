fee33536f3f9d96ca1388f248082db929339becd
DeepakGoyal468 <goyaldeepu468@gmail.com>
2019-09-09 00:51:29 +0530

    url change

--- urbanpiperApp/urls.py
+++ urbanpiperApp/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 urlpatterns = [
-    path('news', include('newsapp.urls')),
+    path('news/', include('newsapp.urls')),
     path('admin/', admin.site.urls),
 ]
